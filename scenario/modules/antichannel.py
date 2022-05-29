aceda618ddf83a8ee1ef4a285e98e220ec7cdecb
Imran95942 <halil-223@mail.ru>
2022-05-29 13:26:07 +0300

    Update antichannel.py

--- scenario/modules/antichannel.py
+++ scenario/modules/antichannel.py
@@ -1,76 +1,63 @@
-"""
-BSD 2-Clause License
+import asyncio
 
-Copyright (C) 2017-2019, Paul Larsen
-Copyright (C) 2021-2022, Awesome-RJ, <https://github.com/Awesome-RJ>
-Copyright (c) 2021-2022, Yūki • Black Knights Union, <https://github.com/Awesome-RJ/CutiepiiRobot>
-
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-"""
-import html
-from telegram import Update
-from telegram.ext import filters, CallbackContext
-
-from scenario import CUTIEPII_PTB
-from scenario.modules.disable import DisableAbleCommandHandler
-from scenario.modules.helper_funcs.anonymous import user_admin
-from scenario.modules.sql.antichannel_sql import antichannel_status, disable_antichannel, enable_antichannel
-
-@user_admin
-async def set_antichannel(update: Update, context: CallbackContext):
-    message = update.effective_message
-    chat = update.effective_chat
-    args = context.args
-    if len(args) > 0:
-        s = args[0].lower()
-        if s in ["yes", "on"]:
-            enable_antichannel(chat.id)
-            await message.reply_html(f"Enabled antichannel in {html.escape(chat.title)}")
-        elif s in ["off", "no"]:
-            disable_antichannel(chat.id)
-            await message.reply_html(f"Disabled antichannel in {html.escape(chat.title)}")
-        else:
-            await update.effective_message.reply_text(f"Unrecognized arguments {s}")
-        return
-    await message.reply_html(
-        f"Antichannel setting is currently {antichannel_status(chat.id)} in {html.escape(chat.title)}"
-    )
+from loguru import logger
+from pyrogram import errors
+from pyrogram.raw import types, functions
+from pyrogram.raw.base import Update
+from .. import pgram as Client
+from .. import pgram as client 
 
 
-async def eliminate_channel(update: Update, context: CallbackContext):
-    message = update.effective_message
-    chat = update.effective_chat
-    bot = context.bot
-    if not antichannel_status(chat.id):
-        return
-    if message.sender_chat and message.sender_chat.type == "channel" and not message.is_automatic_forward:
-        await message.delete()
-        sender_chat = message.sender_chat
-        await bot.ban_chat_sender_chat(sender_chat_id=sender_chat.id, chat_id=chat.id)
 
+@Client.on_raw_update()
+async def message_handler(client: Client, update: Update, _, chats: dict):
+    while True:
+        try:
+            # Check for message that are from channel
+            if (not isinstance(update, types.UpdateNewChannelMessage) or
+                    not isinstance(update.message.from_id, types.PeerChannel)):
+                return
 
-ANTICHANNEL_HANDLER = DisableAbleCommandHandler("antichannel", set_antichannel, filters=filters.ChatType.GROUPS)
+            # Basic data
+            message = update.message
+            chat_id = int(f"-100{message.peer_id.channel_id}")
+            channel_id = int(f"-100{message.from_id.channel_id}")
 
+            # Check for linked channel
+            if ((message.fwd_from and 
+                 message.fwd_from.saved_from_peer == message.fwd_from.from_id == message.from_id) or
+                channel_id == chat_id):
+                return
 
-CUTIEPII_PTB.add_handler(ANTICHANNEL_HANDLER)
+            # Delete the message sent by channel and ban it.
+            await client.send(
+                functions.channels.EditBanned(
+                    channel=await client.resolve_peer(chat_id),
+                    participant=await client.resolve_peer(channel_id),
+                    banned_rights=types.ChatBannedRights(
+                        until_date=0,
+                        view_messages=True,
+                        send_messages=True,
+                        send_media=True,
+                        send_stickers=True,
+                        send_gifs=True,
+                        send_games=True,
+                        send_polls=True,
+                    )
+                )
+            )
+            await client.delete_messages(chat_id, message.id)
+            logger.debug(f"Banned channel {channel_id} from group {chat_id}")
+
+            break
+        except errors.FloodWait as e:
+            logger.debug(f"{e}, retry after {e.x} seconds...")
+            await asyncio.sleep(e.x)
+        except errors.ChatAdminRequired:
+            pass
+        except:  # noqa
+            logger.exception("An exception occurred in message_handler")
+        
+
+__help__ = "This module is to ban channel sender, it's global and true only! Will add toggle function later"
+__mod_name__ = "Antichannel"
